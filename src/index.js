a08c4e995c6779f4d75c4da795080c1838ce2b77
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-18 15:14:33 +0200

    url check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './css/styles.css';
 let formRef = document.querySelector('.search-form');
 let inputRef = document.querySelector('#text');
 let searchButton = document.querySelector('#submit');
-const API_URL = "https://pixabay.com/api/";
  
 formRef.addEventListener('submit', searchData);
  
