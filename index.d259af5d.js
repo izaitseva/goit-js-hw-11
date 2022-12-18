8ab71cbee95657d78b5137bb66b2fb45a90cc27c
izaitseva <75270978+izaitseva@users.noreply.github.com>
2022-12-18 12:02:52 +0000

    Deploying to gh-pages from @ izaitseva/goit-js-hw-11@eedc5695ceae88c8624dd5fb5872e60eda0a6c64 🚀

---
+++ index.d259af5d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-form");document.querySelector("#text"),document.querySelector("#submit");e.addEventListener("submit",(function(e){e.preventDefault()})),fetch("https://pixabay.com/api/").then((function(e){return e.json()})).then((function(e){return console.log(e)})),console.log("test")}();
+//# sourceMappingURL=index.d259af5d.js.map
