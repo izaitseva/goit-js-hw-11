65025663d9734af18c8c45fd7053dd5577775900
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-20 00:03:48 +0200

    css styles

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import { getImages } from './getImages';
 let formRef = document.querySelector('.search-form');
 let inputRef = document.querySelector('[name=searchQuery]');
 let gallery = document.querySelector('.gallery');
+let btnEl = document.querySelector('.load-more');
+
 
 formRef.addEventListener('submit', searchData);
 
 function searchData(event) {
     event.preventDefault();
     let value = inputRef.value;
-
+    
     getImages(value).then(data => {
         if (data.length === 0) {
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
@@ -25,27 +27,29 @@ function searchData(event) {
 
 function renderImages(images) {
     const markup = images.map(item => `
-        <div class="photo-card">
-            <img src="${item.webformatURL}" alt="" loading="lazy" />
-            <div class="info">
-                <p class="info-item">
-                    <b>Likes</b>
-                    <span>${item.likes}</span>
-                </p>
-                <p class="info-item">
+    <div class="photo-card">
+    <img src="${item.webformatURL}" alt="" loading="lazy" />
+    <div class="info">
+    <p class="info-item">
+    <b>Likes</b>
+    <span>${item.likes}</span>
+    </p>
+    <p class="info-item">
                     <b>Views</b>
                     <span>${item.views}</span>
-                </p>
-                <p class="info-item">
+                    </p>
+                    <p class="info-item">
                     <b>Comments</b>
                     <span>${item.comments}</span>
-                </p>
-                <p class="info-item">
+                    </p>
+                    <p class="info-item">
                     <b>Downloads</b>
                     <span>${item.downloads}</span>
                 </p>
-            </div>
-        </div>
-    `).join('')
+                </div>
+                </div>
+                `).join('')
     gallery.innerHTML = markup
 }
+
+btnEl.addEventListener('click', searchData)
