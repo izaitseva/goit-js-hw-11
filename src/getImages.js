8da77916b0853fb0bfa99812ad845dfb6139c8a7
Iryna Zaitseva <zaitseva02@gmail.com>
2022-12-21 23:17:25 +0200

    styles css

--- src/getImages.js
+++ src/getImages.js
@@ -1,33 +1,22 @@
 import axios from "axios";
-const API_KEY = '12755760-d2e38158efcb067b906f81c79';
-const URL = 'https://pixabay.com/api?';
+const KEY = '12755760-d2e38158efcb067b906f81c79';
+const PER_PAGE = 40;
+const SEARCH_PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
 
-export async function getImages(search) {
-    try {
-        let response = await axios.get(URL, {
-            params: {
-                key: API_KEY,
-                q: search,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-            }
-        });
-
-        let hits = response.data.hits.map(el => {
-            return {
-                webformatURL: el.webformatURL,
-                largeImageURL: el.largeImageURL,
-                tags: el.tags,
-                likes: el.likes,
-                views: el.views,
-                comments:el.comments,
-                downloads:el.downloads,
-            }
-        });
-        return hits;
-
-    } catch (error) {
-        console.log(error)
-    }
+export async function getImages(search, page) {
+    const URL = `https://pixabay.com/api?key=${KEY}&${SEARCH_PARAMS}&q=${search}&page=${page}&per_page=${PER_PAGE}`;
+    const response = await axios.get(URL);
+    const images = response.data.hits.map(el => {
+        return {
+            webformatURL: el.webformatURL,
+            largeImageURL: el.largeImageURL,
+            tags: el.tags,
+            likes: el.likes,
+            views: el.views,
+            comments: el.comments,
+            downloads: el.downloads,
+            totalHits: el.totalHits
+        }
+    });
+    return images;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './css/styles.css';
+// import './css/styles.css';
+import './css/styles.css'
 import Notiflix from 'notiflix';
 import { getImages } from './getImages';
 
@@ -6,19 +7,24 @@ let formRef = document.querySelector('.search-form');
 let inputRef = document.querySelector('[name=searchQuery]');
 let gallery = document.querySelector('.gallery');
 let btnEl = document.querySelector('.load-more');
-
+let page = 1;
+let searchString;
 
 formRef.addEventListener('submit', searchData);
 
 function searchData(event) {
     event.preventDefault();
-    let value = inputRef.value;
-    
-    getImages(value).then(data => {
+    searchString = inputRef.value;
+    gallery.innerHTML = '';
+
+    page = 1;
+    getImages(searchString, page).then(data => {
+
         if (data.length === 0) {
             Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            gallery.innerHTML = '';
+            btnEl.style.display = 'none'
         } else {
+            btnEl.style.display = "";
             renderImages(data);
         }
         console.log(data);
@@ -28,28 +34,41 @@ function searchData(event) {
 function renderImages(images) {
     const markup = images.map(item => `
     <div class="photo-card">
-    <img src="${item.webformatURL}" alt="" loading="lazy" />
-    <div class="info">
-    <p class="info-item">
-    <b>Likes</b>
-    <span>${item.likes}</span>
-    </p>
-    <p class="info-item">
-                    <b>Views</b>
+        <img src="${item.webformatURL}" alt="" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b>
+                    <span>${item.likes}</span>
+            </p>
+            <p class="info-item">
+                <b>Views</b>
                     <span>${item.views}</span>
-                    </p>
-                    <p class="info-item">
-                    <b>Comments</b>
+            </p>
+            <p class="info-item">
+                <b>Comments</b>
                     <span>${item.comments}</span>
-                    </p>
-                    <p class="info-item">
-                    <b>Downloads</b>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b>
                     <span>${item.downloads}</span>
-                </p>
-                </div>
-                </div>
-                `).join('')
-    gallery.innerHTML = markup
+            </p>
+        </div>
+    </div>
+    `).join('')
+    gallery.insertAdjacentHTML("beforeend", markup);
 }
 
-btnEl.addEventListener('click', searchData)
+btnEl.addEventListener('click', loadMore)
+
+function loadMore() {
+    page++
+    getImages(searchString, page)
+        .then(data => {
+            if (data.length === 0) {
+                btnEl.style.display = 'none'
+                Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            } else {
+                renderImages(data)
+            }
+        })
+}
